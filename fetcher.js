a78f06834fd3f9c09eb2c6ca2f5207f74f7f1118
maddie <lee.maddie@gmail.com>
2023-09-22 13:38:08 -0400

    Testing request and getting file size via length of body

--- fetcher.js
+++ fetcher.js
@@ -8,13 +8,14 @@ It should take two command line arguments:
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-console.log('testing filePath.slice:', url + filePath.slice(1));
+// format given is: url ends in / + file path begins with ./
+console.log('testing filePath.slice:', url + filePath.slice(2));
 
-// request(url + filePath.slice(1), (error, response, body) => {
-//   console.log('error:', error);
-//   console.log('statusCode:', response && response.statusCode);
-//   console.log('body:', body);
-// });
+request(url + filePath.slice(2), (error, response, body) => {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body length:', body.length, '(1 character is equal to 1 byte)');
+});
 
 /*
 Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.

20245197fcd67125b0fd92608efcd464d6ef2e23
maddie <lee.maddie@gmail.com>
2023-09-22 16:24:48 -0400

    Implemented download message and error handling

--- fetcher.js
+++ fetcher.js
@@ -9,14 +9,11 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 // format given is: url ends in / + file path begins with ./
-console.log('testing filePath.slice:', url + filePath.slice(2));
-
 request(url + filePath.slice(2), (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  console.log('body length:', body.length, '(1 character is equal to 1 byte)');
+  if (error) {
+    console.log('error:', error);
+  } else {
+    // 1 char = 1 byte
+    console.log(`Downloaded and saved ${body.length} bytes to ${filePath}.`);
+  }
 });
-
-/*
-Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.
-*/

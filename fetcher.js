51e8699b44f164601d4bff198f96d3309b083ea8
maddie <lee.maddie@gmail.com>
2023-09-22 13:01:18 -0400

    Added test code for assigning variables directly to CLI arguments

--- fetcher.js
+++ fetcher.js
@@ -5,8 +5,12 @@ It should take two command line arguments:
   * a local file path
 */
 
-const args = process.argv.slice(2);
+const args = process.argv;
 console.log('args:', args);
+const url = process.argv[2];
+console.log('url:', url);
+const filePath = process.argv[3];
+console.log('filePath:', filePath);
 
 /*
 Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.

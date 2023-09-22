6eb0d30f38399646f5c9cb42fe0e03325fa471d7
maddie <lee.maddie@gmail.com>
2023-09-22 12:36:45 -0400

    Initialized with request and app objectives

---
+++ fetcher.js
@@ -0,0 +1,10 @@
+const request = require('request');
+/*
+It should take two command line arguments:
+  * a URL
+  * a local file path
+*/
+
+/*
+Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.
+*/

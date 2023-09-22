4e747e3ba0b2ae0ff46be22eba3072111fc6d0f4
maddie <lee.maddie@gmail.com>
2023-09-22 16:46:17 -0400

    Refactored to make functions modular

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const fs = require("fs");
+const { fileURLToPath } = require('url');
 
 /*
 It should take two command line arguments:
@@ -15,12 +16,19 @@ request(url, (error, response, body) => {
     console.log('error:', error);
   } else {
     // need to download file to file path!!!
-    fs.writeFile(filePath, body, 'utf8', (err) => {
-      if (err) {
-        console.log('error:', err);
-      }
-      // 1 char = 1 byte
-      console.log(`Downloaded and saved ${body.length} bytes to ${filePath}.`);
-   });
+    // check if file already exists - fs.access(filePath)
+
+    downloadFile(filePath, body);
   }
 });
+
+const downloadFile = function (path, file) {
+  fs.writeFile(path, file, 'utf8', (err) => {
+    if (err) {
+      console.log('error:', err);
+    }
+    
+    // 1 char = 1 byte
+    console.log(`Downloaded and saved ${file.length} bytes to ${path}.`);
+ });
+};
